0b32024f7a82552a7f9a73c76267e58b4c4bb24c
0wan1001 <rotk125@naver.com>
2026-08-28 15:32:02 +0900

    [Feat] ShoppingCart 추가

---
+++ ConsoleApp1/ConsoleApp1/ShoppingCart.cs
@@ -0,0 +1,16 @@
+﻿public class ShoppingCart
+{
+    public Menu menu;
+    protected int count { get; set; }
+    
+    public ShoppingCart(Menu _menu, int _count)
+    {
+        menu = _menu;
+        count = _count;
+    }
+
+    public int GetPrice()
+    {
+        return menu.CalculatePrice() * count;
+    }
+}

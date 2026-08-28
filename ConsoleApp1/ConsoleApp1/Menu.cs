a42f49ef681ba33e674c6c30ee8539df2fdb1f4a
0wan1001 <rotk125@naver.com>
2026-08-28 15:28:39 +0900

    [Feat] Menu 클래스 , 열거형 카테고리 추가

---
+++ ConsoleApp1/ConsoleApp1/Menu.cs
@@ -0,0 +1,21 @@
+﻿public enum Category
+{
+    Drink,
+    Bread,
+    Dailynecessities
+}
+
+public abstract class Menu
+{
+    public string name;
+    protected int price { get; set; }
+    public Category category;
+    
+    public Menu(string _name, int _price, Category _category)
+    {
+        name = _name;
+        price = _price;
+        category = _category;
+    }
+    public abstract int CalculatePrice();
+}
